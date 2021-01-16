14cd2623112fc7870393bdcd948ee6dfabaeddbf
saltcoffee <ybell1028@gmail.com>
2021-01-16 15:51:27 +0900

    Exam13305 소방수

--- src/Practice/prodRandNum.java
+++ src/Practice/prodRandNum.java
@@ -5,10 +5,10 @@ import java.text.DecimalFormat;
 public class prodRandNum {
     public static DecimalFormat df;
     public static void main(String[] args) {
-        df = new DecimalFormat("0.000");
-        for(int i = 0; i < 10000; ++i){
-            String rand = df.format(Math.random() * 100);
-            System.out.println(rand);
+        df = new DecimalFormat("0");
+        for(int i = 0; i < 8; ++i){
+            String rand = df.format(Math.random() * 6);
+            System.out.print(rand + " ");
         }
     }
 }
