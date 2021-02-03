23a6723d4d1c587e4d718fcf59ab3178a96f2b67
saltcoffee <ybell1028@gmail.com>
2021-02-03 13:32:31 +0900

    간단한 두문제 완료

--- src/Practice/prodRandNum.java
+++ src/Practice/prodRandNum.java
@@ -6,10 +6,19 @@ public class prodRandNum {
     public static DecimalFormat df;
     public static void main(String[] args) {
         df = new DecimalFormat("0");
-        for(int i = 0; i < 500000; ++i){
-            String rand1 = df.format(Math.random() * 5 + 1);
-            String rand2 = df.format(Math.random() * 300000);
-            System.out.println(rand1 + " " + rand2);
+        System.out.println(1000);
+        for(int i = 0; i < 1000; ++i){
+            boolean[] tri = new boolean[4];
+            for(int j = 1; j <= 3; ++j){
+                String rand = df.format(Math.random() * 2 + 1);
+                int idx = Integer.parseInt(rand);
+                if(tri[idx]) j--;
+                else {
+                    tri[idx] = true;
+                    System.out.print(rand + " ");
+                }
+            }
+            System.out.println();
         }
     }
 }
--- src/Test/Exam2869.java
+++ src/Test/Exam2869.java
@@ -1,20 +1,33 @@
 package Test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Exam2869 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int A, B, V;
 
-        A = sc.nextInt();
-        B = sc.nextInt();
-        V = sc.nextInt();
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-        int pd = A - B; // 하루에 움직이는 거리
-
-        int result = (V - A) / pd + 1;
+        int result;
+        int pd = A - B; // 미끄러지는 경우
+        int remain = V - A; // 출발해서 미끄러지지 않는 경우
+        if(remain <= 0){
+            result = 1;
+        }
+        else if(A > V - pd){
+            result = 2;
+        } else {
+            result = remain / pd + 1;
+        }
 
         System.out.println(result);
     }
