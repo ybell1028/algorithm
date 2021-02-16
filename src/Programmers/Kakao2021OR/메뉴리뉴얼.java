177b55c5086fabedf58315e9e8ef978228eec4cd
saltcoffee <ybell1028@gmail.com>
2021-02-16 20:32:53 +0900

    메뉴 리뉴얼 풀이완료

--- src/Programmers/Kakao2021OR/메뉴리뉴얼.java
+++ src/Programmers/Kakao2021OR/메뉴리뉴얼.java
@@ -1,72 +1,88 @@
 package Programmers.Kakao2021OR;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class 메뉴리뉴얼 {
     static class Solution {
+        HashMap<String, Integer> hm;
+        StringBuilder sb = new StringBuilder();
+        LinkedList<String> popular = new LinkedList<>();
+        int var;
         public String[] solution(String[] orders, int[] course) {
-            int[] menu = new int[26];
-
-            int max = 0;
-
-            for(int i = 0; i < orders.length; ++i){
-                for(int j = 0; j < orders[i].length(); ++j){
-                    menu[orders[i].charAt(j) - 65]++;
+            //손님이 주문한 단품 요리 리스트인 orders로 course에 들어있는 구성 수를 가지는 경우의 수를 구하자.
+            for(int i = 0; i < course.length; ++i){
+                hm = new HashMap<>();
+                var = course[i];
+                for (int j = 0; j < orders.length; ++j) {
+                    combi(orders[j], 0, orders[j].length(), 0);
                 }
+                searchPopular();
             }
-
-            for(int i = 0; i < menu.length; ++i){
-                max = Math.max(max, menu[i]); // 가장 많은 사람들이 고른 메뉴
-            }
-            //요리 5개를 주문한 손님이 1명 있지만, 최소 2명 이상의 손님에게서 주문된 구성만 코스요리 후보에 들어가므로
-            //요리 5개로 구성된 코스요리는 새로 추가하지 않습니다.
-
-            Queue<String>[] qArr = new LinkedList[max + 1];
-            LinkedList<String> result = new LinkedList<>();
-            for(int i = 0; i <= max; ++i){
-                qArr[i] = new LinkedList<>();
-            }
-
-            int temp = max;
-
-            while(max >= 2){
-                for(int i = 0; i < menu.length; ++i){
-                    if(menu[i] >= max){
-                        qArr[max].add(Character.toString((char)(i + 65)));
-                    }
-                }
-                max--;
-            }
-
-            for(int i = 0; i < course.length; ++i){ // 만드려고 하는 코스 수만큼
-                int var = course[i]; // 요리 가짓수
-                max = temp;
-                while(max >= 2){
-                    if(qArr[max].size() >= var){
-                        StringBuilder sb = new StringBuilder();
-                        while(!qArr[max].isEmpty()){
-                            sb.append(qArr[max].poll());
-                            if(sb.length() == var){
-                                result.add(sb.toString());
-                            }
-                        }
-                    }
-                    max--;
-                }
-            }
-
-            String[] answer = new String[result.size()];
-            for(int i = 0; i < result.size(); ++i){
-                answer[i] = result.poll();
+            String[] answer = new String[popular.size() + 1];
+            int idx = 0;
+            while(!popular.isEmpty()){
+                answer[idx] = popular.poll();
+                System.out.println(answer[idx]);
+                idx++;
             }
             return answer;
+        }
+
+        public void combi(String order, int insert, int olen, int vlen){
+            if(olen < insert) {
+                sb.delete(0, sb.length() - 1);
+                return;
+            }
+            if(var <= vlen) {
+                char[] cArr = sb.toString().toCharArray();
+                Arrays.sort(cArr);
+                StringBuilder sorted = new StringBuilder();
+                for(int i = 0; i < cArr.length; ++i){
+                    sorted.append(Character.toString(cArr[i]));
+                }
+                String key = sorted.toString();
+                hm.put(key, hm.getOrDefault(key, 0) + 1);
+                return;
+            }
+            for(int i = insert; i < olen; ++i){
+                if(sb.length() == 0 && olen - i < var) break;
+                sb.append(Character.toString(order.charAt(i)));
+                combi(order, i + 1, olen, vlen + 1);
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+
+        public void searchPopular(){
+            int max = 2;
+            Iterator<Map.Entry<String, Integer>> entries = hm.entrySet().iterator();
+            while(entries.hasNext()){
+                Map.Entry<String, Integer> entry = entries.next();
+                if(max <= entry.getValue())
+                    max = entry.getValue();
+            }
+            entries = hm.entrySet().iterator();
+            while(entries.hasNext()){
+                Map.Entry<String, Integer> entry = entries.next();
+                if(entry.getValue() == max){
+                    popular.add(entry.getKey());
+                }
+            }
+
+            for(int i = 0; i < popular.size() - 1; ++i){
+                for(int j = i + 1; j < popular.size(); ++j){
+                    if(popular.get(i).compareTo(popular.get(j)) > 0){
+                        String temp = popular.get(i);
+                        popular.set(i, popular.get(j));
+                        popular.set(j, temp);
+                    }
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        String[] orders = {"XYZ", "XWY", "WXA"};
         int[] course = {2,3,4};
         System.out.println(s.solution(orders, course));
     }
