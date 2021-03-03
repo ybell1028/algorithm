b1127c84482e13e87c7c6b7b29cee0a017ac2d01
황종훈 <ybell1028@gmail.com>
2021-03-03 13:19:27 +0900

    [기타] 알고리즘 정리 노트 추가

--- src/Programmers/코딩테스트고득점Kit/Greedy/섬연결하기.java
+++ src/Programmers/코딩테스트고득점Kit/Greedy/섬연결하기.java
@@ -92,3 +92,48 @@ public class 섬연결하기 {
         System.out.println(s.solution(n, costs));
     }
 }
+
+//프로그래머스 모범답안
+//import java.util.Arrays;
+//
+//class Solution
+//{
+//    public int solution(int n, int[][] costs)
+//    {
+//        int sum = 0;
+//        int[] island = new int[n];
+//
+//        // Step1. 모든 값이 자기 자신을 가리키도록 초기화
+//        for(int i = 0; i < n; i++)
+//            island[i] = i;
+//
+//        // Step2. 모든 간선들을 가중치를 기준으로 오름차순으로 정렬
+//        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
+//
+//        for(int i = 0; i < costs.length; i++)
+//        {
+//            // Step3. 양 끝점이 다르다면 Unite해주고 가중치를 sum에 더해준다.
+//            if(find(island, costs[i][0]) != find(island, costs[i][1]))
+//            {
+//                unite(island, costs[i][0], costs[i][1]); // 합병
+//                sum += costs[i][2];
+//            }
+//        }
+//
+//        return sum;
+//    }
+//
+//    private int find(int[] island, int x)
+//    {
+//        if(island[x]== x)
+//            return x;
+//        return find(island, island[x]);
+//    }
+//
+//    private void unite(int[] island, int x, int y)
+//    {
+//        int a = find(island, x);
+//        int b = find(island, y);
+//        island[a] = b;
+//    }
+//}
