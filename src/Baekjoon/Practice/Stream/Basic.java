4d60ea2001676942fc2f4071bec40ad11d26ade5
황종훈 <ybell1028@gmail.com>
2021-04-24 12:21:01 +0900

    Add : [기타] 스트림 공부용 패키지 생성

---
+++ src/Baekjoon/Practice/Stream/Basic.java
@@ -0,0 +1,18 @@
+package Baekjoon.Practice.Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Basic {
+    public static void main(String[] args) {
+        //매핑 - 중간처리 기능으로 스트림의 요소를 다른 요소로 대체하는 작업을 한다.
+        
+        //flatXXX() : 요소를 대체하는 복수개의 요소들로 구성된 새로운 스트림을 리턴
+        List<String> input1 = Arrays.asList("java8 lambda","stream mapping");
+        input1.stream()
+                .flatMap(data -> Arrays.stream(data.split(" ")))
+                .forEach(System.out::println);
+
+        System.out.println();
+    }
+}
