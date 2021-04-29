6cd68918a7cfddbfdc8d6783d81e5d21ccd17b8c
황종훈 <ybell1028@gmail.com>
2021-04-29 22:11:35 +0900

    Add : [프로그래머스] 문제풀이 중

--- src/Baekjoon/Practice/prodRandNum.java
+++ src/Baekjoon/Practice/prodRandNum.java
@@ -6,10 +6,18 @@ public class prodRandNum {
     public static DecimalFormat df;
     public static void main(String[] args) {
         df = new DecimalFormat("0");
-        System.out.println(1000);
-        for(int i = 0; i < 10; ++i){
-            String rand = df.format(Math.random() * 10);
-            System.out.println(rand);
+        int head = 0;
+        int tail = 0;
+        for(int i = 0; i < 100; ++i){
+            if(head >= 50) System.out.print(")");
+            else if(tail >= 50) System.out.print("(");
+            else {
+                String s = (int)(Math.random() * 2) + 1 == 1 ? "(" : ")";
+                if(s.equals("(")) head++;
+                else tail ++;
+                System.out.print(s);
+            }
         }
+//        System.out.println(Math.random() * 2 + 1);
     }
 }
