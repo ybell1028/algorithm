c4418077559c5c4496d4c38289cf8a5091c6e7d8
황종훈 <ybell1028@gmail.com>
2021-04-30 15:05:51 +0900

    Add : [Programmers] 괄호변환 풀이완료

--- src/Programmers/YAPP/Week8/괄호변환.java
+++ src/Programmers/YAPP/Week8/괄호변환.java
@@ -1,93 +1,67 @@
 package Programmers.YAPP.Week8;
 
-//시작 8시 55분
 public class 괄호변환 {
     static class Solution {
-        static StringBuilder u;
-        static StringBuilder v;
-        static StringBuilder sb;
+        StringBuilder sb = new StringBuilder();
         public String solution(String p) {
-            if (p.equals("")) return p;
-            sb = new StringBuilder();
-            u = new StringBuilder();
-            v = new StringBuilder(p);
+            return getCorrect(p);
+        }
 
-            while(true) {
-                boolean csb = correct(sb);
-                if (sb.length() == p.length() && csb) break;
-                if (csb) u = new StringBuilder();
-                else sb = new StringBuilder();
-                if(v.length() <= 0){
-                    v.append(sb);
-                    sb = new StringBuilder();
-                } else {
-                    makeU(v);
-                }
-                if (correct(u)) {
-                    sb.append(u);
-                } else {
-                    v = new StringBuilder(transform(u, v.toString()));
-                    System.out.println("sblen = " + sb.length());
-                    System.out.println("vlen = " + v.length());
-                    System.out.println("ulen = " + u.length());
-                    sb.append(v);
-                    u = new StringBuilder();
+        public static int makeU(String p){
+            String[] splited = p.split("");
+            int cnt = 0;
+            int idx = 0;
+            for(int i = 0; i < splited.length; ++i){
+                if(splited[i].equals("(")) cnt++;
+                else cnt--;
+                if(cnt == 0){
+                    idx = i + 1;
+                    break;
                 }
             }
+            return idx;
+        }
 
-//            if(v.toString().equals("")) sb.append(u);
+        public static boolean correct(String u){
+            String[] splited = u.split("");
+            int cnt = 0;
+            for(String s : splited){
+                if(s.equals("(")) cnt++;
+                else cnt--;
+                if(cnt < 0) return false;
+            }
+            return true;
+        }
 
+        public String getCorrect(String p){
+            int idx = makeU(p);
+            String u = p.substring(0, idx);
+            String v = p.substring(idx);
+
+            if(correct(u)) sb.append(u);
+            else {
+                sb.append("(");
+                if(!v.equals("")){
+                    getCorrect(v);
+                    v = "";
+                }
+                sb.append(")");
+                sb.append(cutReverse(u));
+            }
+            if(!v.equals("")){
+                getCorrect(v);
+            }
             return sb.toString();
         }
 
-        public static void makeU(StringBuilder v){
-            StringBuilder temp = new StringBuilder(v);
-            int front = 0;
-            int back = 0;
-            temp.delete(1, v.length());
-            v.delete(0, 1);
-            front++;
-            String fs = temp.toString();
-            u.append(fs);
-            while(front > back){
-                temp = new StringBuilder(v);
-                temp.delete(1, v.length());
-                v.delete(0, 1);
-                if(temp.toString().equals(fs)) front++;
-                else back++;
-                u.append(temp);
+        public String cutReverse(String p){
+            String[] split = p.split("");
+            StringBuilder sb = new StringBuilder();
+            for(int i = 1; i < split.length - 1; i++){
+                if(split[i].equals("(")) sb.append(")");
+                else sb.append("(");
             }
-        }
-
-        public static boolean correct(StringBuilder u){
-            if(u.toString().equals("")) return false;
-            else if(u.substring(0, 1).equals(")")) return false; // 무조건 시작은 '('
-            StringBuilder temp = new StringBuilder(u);
-            int head = 0;
-            int tail = 0;
-            for(int i = 0; i < u.length(); ++i){
-                temp.delete(i + 1, temp.length());
-                temp.delete(0, i);
-                if(temp.toString().equals("(")) head++;
-                else tail++;
-                if(tail > head) return false;
-                temp = new StringBuilder(u);
-            }
-            return head == tail ? true : false;
-        }
-
-        public static String transform(StringBuilder u, String v){
-            StringBuilder temp = new StringBuilder();
-            temp.append("(" + v + ")");
-            u.delete(0, 1);
-            u.delete(u.length() - 1, u.length());
-            temp.append(u.reverse());
-//            if(u.length() > 0){
-//                u.delete(0, 1);
-//                u.delete(u.length() - 1, u.length());
-//                temp.append(u.reverse());
-//            }
-            return temp.toString();
+            return sb.toString();
         }
 
         public static void main(String[] args) {
@@ -99,7 +73,7 @@ public class 괄호변환 {
             String s6 = "(()((()())))";
 
             Solution s = new Solution();
-            System.out.println(s.solution(s5));
+            System.out.println(s.solution(s4));
         }
     }
 }
