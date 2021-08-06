938ae2fae61237bf5f8fe96438e5dd1f4ac7b5fa
황종훈 <ybell1028@gmail.com>
2021-08-06 22:22:00 +0900

    Add : [프로그래머스] 메뉴리뉴얼 풀이완료

---
+++ src/Programmers/YAPP/Week18/메뉴리뉴얼.java
@@ -0,0 +1,53 @@
+package Programmers.YAPP.Week18;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 메뉴리뉴얼 {
+    class Solution {
+        public final int MAX = 11;
+
+        public String[] solution(String[] orders, int[] course) {
+            Arrays.sort(orders);
+
+            int max = course[course.length - 1];
+
+            Map<String, Integer> hm[] = new HashMap[MAX];
+            for(int i = 0; i < MAX; ++i){
+                hm[i] = new HashMap<>();
+            }
+
+            StringBuilder sb;
+            int len;
+            for(String order : orders) {
+                String[] temp = order.split("");
+                Arrays.sort(temp);
+                for(int i = 0; i < (1 << order.length()); ++i){
+                    sb = new StringBuilder();
+                    for(int j = 0; j < order.length(); ++j) {
+                        if((i & (1 << j)) > 0) sb.append(temp[j]);
+                    }
+                    len = sb.length();
+                    if(len < 2) continue;
+                    String key = sb.toString();
+                    hm[len].put(key, hm[len].getOrDefault(key, 0) + 1);
+                }
+            }
+
+            Set<String> ts = new TreeSet<>();
+
+            for(int i : course){
+                List<Map.Entry<String, Integer>> sortedMenu = new ArrayList<>(hm[i].entrySet());
+                if(sortedMenu.isEmpty()) continue;
+                sortedMenu.sort(Map.Entry.comparingByValue(Collections.reverseOrder()));
+                int maxVal = sortedMenu.get(0).getValue();
+                ts.addAll(sortedMenu.stream()
+                        .filter(e -> e.getValue() > 1 && e.getValue() == maxVal)
+                        .map(Map.Entry::getKey)
+                        .collect(Collectors.toSet())
+                );
+            }
+            return ts.toArray(new String[0]);
+        }
+    }
+}
