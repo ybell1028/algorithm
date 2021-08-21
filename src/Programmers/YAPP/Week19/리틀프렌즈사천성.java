9defe898e2917f95d4869d71202caccf35b3ae5d
황종훈 <ybell1028@gmail.com>
2021-08-21 21:18:37 +0900

    Add : [프로그래머스] 리틀프렌즈사천성 풀이중2

--- src/Programmers/YAPP/Week19/리틀프렌즈사천성.java
+++ src/Programmers/YAPP/Week19/리틀프렌즈사천성.java
@@ -3,17 +3,25 @@ package Programmers.YAPP.Week19;
 import java.util.*;
 import java.util.regex.Pattern;
 
+import static java.util.stream.Collectors.toList;
+
 public class 리틀프렌즈사천성 {
     static class Solution {
+        static int dir = -1; // -1은 시작 0은 수직 1은 좌우
         static int N, M;
         static int[] dr = {-1, 1, 0, 0};
         static int[] dc = {0, 0, -1, 1};
+        static boolean found = false;
         static boolean[][] visited;
         static String[][] newBoard;
-        static int minCost;
+        static Stack<String> route;
+        static Map<String, List<Node>> endMap;
+        static Map<String, List<String>> routeMap;
+        StringBuilder sb;
         public String solution(int m, int n, String[] board) {
-            Map<String, List<Node>> endMap = new HashMap<>();
-            Map<String, Set<String>> routeMap = new HashMap<>();
+            endMap = new HashMap<>();
+            routeMap = new HashMap<>();
+            sb = new StringBuilder();
             M = m;
             N = n;
             newBoard = new String[m][n];
@@ -27,61 +35,121 @@ public class 리틀프렌즈사천성 {
             }
             for(String key : endMap.keySet()){
                 List<Node> endList = endMap.get(key);
+                route = new Stack<>();
                 Node start = endList.remove(0);
                 Node end = endList.remove(0);
-                BFS(newBoard, key, routeMap, start, end);
-
-                for(String rk : routeMap.keySet()){
-                    System.out.println("Key : " + rk);
-                    for(String route : routeMap.get(rk)){
-                        System.out.println("route : " + route);
-                    }
-                }
+                visited = new boolean[m][n];
+                DFS(key, start, end, -1);
             }
-            return "";
+
+            String[] input = endMap.keySet().toArray(new String[0]);
+            int size = endMap.size();
+            Arrays.sort(input);
+            permutation(size, size, new String[size], input, 0, 0);
+
+            if(sb.length() == 0) return "IMPOSSIBLE";
+            return sb.toString();
         }
 
         class Node {
             public int row;
             public int col;
 
-            Node(int row, int col){
+            Node(int row, int col) {
                 this.row = row;
                 this.col = col;
             }
         }
 
+        public void permutation(int n, int r, String[] perms, String[] input, int depth, int flag){
+            if(depth == r){
+                boolean con = true;
+                for(String p : perms) sb.append(p);
+                for(int i = 0; i < perms.length; ++i){
+                    String str = sb.substring(0, i + 1);
+                    List<String> routeList = routeMap.get(perms[i]);
+                    if(routeList == null) con = false;
+                    else if(routeList.size() > 0) {
+                        for(String rt : routeList){
+                            if(!str.contains(rt)) {
+                                con = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(!con) {
+                        sb.delete(0, sb.length());
+                        return;
+                    }
+                }
+                found = true;
+                return;
+            }
+            for(int i = 0; i < n; ++i){
+                if(found) break;
+                if((flag & (1 << i)) == 0){
+                    perms[depth] = input[i];
+                    permutation(n, r, perms, input, depth + 1, flag | 1 << i);
+                }
+            }
+        }
 
-        public void BFS(String[][] board, String key, Map<String, Set<String>> routeMap, Node start, Node dest) {
-            visited = new boolean[M][N];
-            Queue<Node> queue = new LinkedList<>();
-            queue.add(start);
-            while(!queue.isEmpty()){
-                Node now = queue.poll();
-                int r = now.row;
-                int c = now.col;
-                if(visited[r][c]) continue;
-                visited[r][c] = true;
-                if(board[r][c].equals("*")) continue;
-                if(now.row == dest.row && now.col == dest.col){
+        public void DFS(String key, Node now, Node dest, int changed) {
+            if(changed > 1) {
+                //tempSet.remove(tempSet);
+                return;
+            }
+            int r = now.row;
+            int c = now.col;
+            if(dest.row == r && dest.col == c) {
+                List<String> result = route.stream().distinct().collect(toList());
+                if(routeMap.get(key) == null || route.size() < routeMap.get(key).size()) routeMap.put(key, result);
+                route = new Stack<>();
+                return;
+            }
+            if(visited[r][c]) return;
+            visited[r][c] = true;
+            if(newBoard[r][c].equals("*")) return;
+            if(!newBoard[r][c].equals(".") && !newBoard[r][c].equals(key)) {
+                route.push(newBoard[r][c]);
+                List<String> list = routeMap.get(key);
+                if(list != null && route.size() > list.size()) {
+                    route.pop();
                     return;
                 }
-                if(!board[r][c].equals(".") && !board[r][c].equals(key))
-                    routeMap.computeIfAbsent(key, s -> new HashSet<>()).add(board[r][c]);
-                for(int i = 0; i < dr.length; ++i){
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-                    if(nr >= 0 && nr < M && nc >= 0 && nc < N) {
-                        queue.add(new Node(nr, nc));
+            }
+            for(int i = 0; i < dr.length; ++i) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >= 0 && nr < M && nc >= 0 && nc < N) {
+                    int newDir;
+                    if(dr[i] != 0) newDir = 0; // 수직이면 0
+                    else newDir = 1; // 좌우면 1
+                    if(dir == newDir) { // 꺾지 않았을때
+                        DFS(key, new Node(nr, nc), dest, changed);
+                    }
+                    else {
+                        int temp = dir;
+                        dir = newDir;
+                        DFS(key, new Node(nr, nc), dest, changed + 1);
+                        dir = temp;
                     }
                 }
             }
+            visited[r][c] = false;
+            if(!route.isEmpty()) route.pop();
         }
     }
 
     public static void main(String[] args) {
-        String[] board = {"DBA", "C*A", "CDB"};
+        String[] board1 = {"DBA", "C*A", "CDB"};
+        String[] board2 = {".ZI.", "M.**", "MZU.", ".IU."};
+        String[] board3 = {"NRYN", "ARYA"};
+        String[] board4 = {"AB", "BA"};
+        String[] board5 = {"A*", "*A"};
+        String[] board6 = {"KOA", "T.T", "AOK"};
+        String[] board7 = {"ZZEE", "JKCJ", "CKAA"};
         Solution s = new Solution();
-        s.solution(3, 3, board);
+        System.out.println(s.solution(3, 4, board7));
     }
 }
