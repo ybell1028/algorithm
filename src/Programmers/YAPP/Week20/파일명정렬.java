0656f6600d9ee611f44ebe936a99ba230da44047
황종훈 <ybell1028@gmail.com>
2021-08-28 13:47:12 +0900

    Add : [프로그래머스] 파일명정렬 풀이완료

---
+++ src/Programmers/YAPP/Week20/파일명정렬.java
@@ -0,0 +1,63 @@
+package Programmers.YAPP.Week20;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+class Solution {
+    int od = 1;
+
+    class Name implements Comparable{
+        public String origin;
+        public String head;
+        public int number;
+        public int order;
+
+        Name(String origin){
+            this.origin = origin;
+            int i = 0;
+            for(i = 0; i < origin.length(); ++i){
+                if(origin.charAt(i) >= '0' && origin.charAt(i) <= '9'){
+                    break;
+                }
+            }
+            this.head = origin.substring(0, i).toLowerCase();
+            int j = i;
+            for(j = i; j < origin.length(); ++j){
+                if(origin.charAt(j) < '0' || origin.charAt(j) > '9'){
+                    break;
+                }
+            }
+            this.number = Integer.valueOf(origin.substring(i, j));
+            this.order = od++;
+        }
+
+        @Override
+        public int compareTo(Object obj){
+            Name name = (Name)(obj);
+            if(!head.equals(name.head)){
+                return head.compareTo(name.head);
+            } else if(number != name.number){
+                return number - name.number;
+            } else {
+                return order - name.order;
+            }
+        }
+    }
+
+    public String[] solution(String[] files) {
+        Name[] nameArr = new Name[files.length];
+
+        List<Name> nameList = new ArrayList<>();
+
+        for(int i = 0; i < nameArr.length; ++i){
+            nameList.add(new Name(files[i]));
+        }
+
+        Collections.sort(nameList);
+
+        String[] answer = nameList.stream()
+                .map(n -> n.origin)
+                .toArray(String[]::new);
+        return answer;
+    }
+}
